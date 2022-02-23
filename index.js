7c887a68943ee818ac83899840ebc58fafac88f8
Ilya <milyavskyfamily@gmail.com>
2022-02-23 18:44:41 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, event= "surprise") {
+    let newCards = [];
+    for (let c = 0; c < cards.length; c++) {
+    newCards.push(`Thank you, ${cards[c]}, for the wonderful ${event} gift!`);
+    debugger;
+    } 
+    
+    return newCards;
+}
+console.log(writeCards(cards))
+
+
+let count = 10;
+function countDown(count){
+   console.log(count);
+    do {
+       count -= 1 ;
+       console.log(count);
+   } while (count > 0);
+}
+console.log(countDown(count));
